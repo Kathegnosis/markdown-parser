cd73c87dd30cfe4f0af393741855003bb372fbb8
LJYhu <80735847+LJYhu@users.noreply.github.com>
2022-05-06 19:43:18 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -4,3 +4,4 @@ cp GradingTests.java markdown-parser/
 cd markdown-parser
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
 java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
+echo "HELLO WORLD"
